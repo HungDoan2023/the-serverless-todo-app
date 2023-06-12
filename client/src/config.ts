4f300a1861c517ca0b97c33799934e2f00a3b83b
HungDoan2023 <cukku2022@gmail.com>
2023-06-12 09:02:53 +0700

    Fork and update business logic from Course 04 Udacity

---
+++ client/src/config.ts
@@ -0,0 +1,11 @@
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = '5yvbbb7fea'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  // TODO: Create an Auth0 application and copy values from it into this map. For example:
+  // domain: 'dev-nd9990-p4.us.auth0.com',
+  domain: 'dev-24rmhuqbivfbg1bo.us.auth0.com',            // Auth0 domain
+  clientId: 'EKefCMG0l2uAzPdLXguYGJLVH1f6Y1yY',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
